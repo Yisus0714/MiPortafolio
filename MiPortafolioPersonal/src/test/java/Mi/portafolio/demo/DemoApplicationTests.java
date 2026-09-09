6db2fa828fc744f70e1ee46920a40e63442e6839
Yisus0714 <130243464+Yisus0714@users.noreply.github.com>
2026-09-08 17:12:31 -0700

    Add files via upload

---
+++ MiPortafolioPersonal/src/test/java/Mi/portafolio/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Mi.portafolio.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
